87f08d3d8c9972bcc1b47bc7eb618f9fda1f8a55
Axel-ne <axel-neugebauer@gmx.net>
2026-06-15 20:59:23 +0200

    change cards

--- scripts/template.js
+++ scripts/template.js
@@ -2,12 +2,12 @@ function getPokemonCardTemplate(pokemon) {
     return /*html*/`
     <section class="card-content-section">
         <p>#${pokemon.id} ${pokemon.name}</p>
-    <div>
+    <div class="pokemon-card-wrapper" style="background-color: ${pokemonTypeColors[pokemon.types[0].type.name]}">
         <img src="${pokemon.sprites.front_default}" alt="">
     </div>
 <div>
-    <img src="" alt="">
-    <img src="" alt="">
+    <p>${pokemon.types[0].type.name}</p>
+    <p></p>
 </div>
 </section>
     `     
